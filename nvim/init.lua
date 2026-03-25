5155e01226bfaf6361244501fadc942f4925faca
Steven Humphrey <catchgithub@33k.co.uk>
2026-03-25 14:32:09 +0000

    Comment out keybindings I don't use

--- nvim/init.lua
+++ nvim/init.lua
@@ -64,9 +64,9 @@ cmp.setup({
     }
   },
   mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
